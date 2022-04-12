1461df10663e3d3d9a0a787af83952d0cad32d09
LaSe28 <mattia.las28@gmail.com>
2022-04-12 17:44:16 +0200

    end with bonus

--- frontend/script.js
+++ frontend/script.js
@@ -2,10 +2,28 @@ new Vue({
   el: '#app',
   data: { 
     arrDischi : [],
+    arrGen: null,
+    genValue : document.querySelector('#gen').value
+
+  },
+  methods: {
+    changedValue () {
+      let genValue = document.querySelector('#gen').value
+
+      this.arrGen = []
+      this.arrDischi.forEach(element => {
+        if(element.genre.toLowerCase() === genValue.toLowerCase()){
+          this.arrGen.push(element)
+          console.log(this.arrGen)
+        } else if (genValue === 'all'){
+          this.arrGen.push(element)
+        }
+      });
+    }
   },
   created() {
     axios.get('http://localhost:8888/php-ajax-dischi/api.php')
-    .then(res => this.arrDischi = res.data)
+    .then(res => this.arrDischi = res.data);
   }
 
 })
